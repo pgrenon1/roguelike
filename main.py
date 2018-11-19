0bf376d50adb83e4950f2a240bf8633a84a1d1b6
Phil <pgrenon.pg@gmail.com>
2018-11-18 19:14:46 -0500

    collision with walls

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
 import libtcodpy as libtcod
-#This is a test - Alvaro
-
-#ay
-
+ 
 #actual size of the window
 SCREEN_WIDTH = 80
 SCREEN_HEIGHT = 50
  
-LIMIT_FPS = 20  #20 frames-per-second maximum
-
+#size of the map
 MAP_WIDTH = 80
 MAP_HEIGHT = 45
-
+ 
+LIMIT_FPS = 20  #20 frames-per-second maximum
+ 
+ 
 color_dark_wall = libtcod.Color(0, 0, 100)
 color_dark_ground = libtcod.Color(50, 50, 150)
-
+ 
+ 
 class Tile:
     #a tile of the map and its properties
     def __init__(self, blocked, block_sight = None):
         self.blocked = blocked
  
         #by default, if a tile is blocked, it also blocks sight
-        block_sight = blocked if block_sight is None else None
+        if block_sight is None: block_sight = blocked
         self.block_sight = block_sight
  
 class Object:
@@ -34,9 +34,10 @@ class Object:
         self.color = color
  
     def move(self, dx, dy):
-        #move by the given amount
-        self.x += dx
-        self.y += dy
+        #move by the given amount, if the destination is not blocked
+        if not map[self.x + dx][self.y + dy].blocked:
+            self.x += dx
+            self.y += dy
  
     def draw(self):
         #set the color and then draw the character that represents this object at its position
@@ -47,6 +48,8 @@ class Object:
         #erase the character that represents this object
         libtcod.console_put_char(con, self.x, self.y, ' ', libtcod.BKGND_NONE)
  
+ 
+ 
 def make_map():
     global map
  
@@ -55,7 +58,34 @@ def make_map():
          [Tile(False) for y in range(MAP_HEIGHT)]
          for x in range(MAP_WIDTH) 
     ]
-
+ 
+    #place two pillars to test the map
+    map[30][22].blocked = True
+    map[30][22].block_sight = True
+    map[50][22].blocked = True
+    map[50][22].block_sight = True
+ 
+ 
+def render_all():
+    global color_light_wall
+    global color_light_ground
+ 
+    #go through all tiles, and set their background color
+    for y in range(MAP_HEIGHT):
+        for x in range(MAP_WIDTH):
+            wall = map[x][y].block_sight
+            if wall:
+                libtcod.console_set_char_background(con, x, y, color_dark_wall, libtcod.BKGND_SET)
+            else:
+                libtcod.console_set_char_background(con, x, y, color_dark_ground, libtcod.BKGND_SET)
+ 
+    #draw all objects in the list
+    for object in objects:
+        object.draw()
+ 
+    #blit the contents of "con" to the root console
+    libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
+ 
 def handle_keys():
     #key = libtcod.console_check_for_keypress()  #real-time
     key = libtcod.console_wait_for_keypress(True)  #turn-based
@@ -80,7 +110,7 @@ def handle_keys():
     elif libtcod.console_is_key_pressed(libtcod.KEY_RIGHT):
         player.move(1, 0)
  
-
+ 
 #############################################
 # Initialization & Main Loop
 #############################################
@@ -89,6 +119,7 @@ libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | 
 libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'python/libtcod tutorial', False)
 libtcod.sys_set_fps(LIMIT_FPS)
 con = libtcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
+ 
 #create object representing the player
 player = Object(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2, '@', libtcod.white)
  
@@ -98,15 +129,15 @@ npc = Object(SCREEN_WIDTH // 2 - 5, SCREEN_HEIGHT // 2, '@', libtcod.yellow)
 #the list of objects with those two
 objects = [npc, player]
  
+#generate map (at this point it's not drawn to the screen)
+make_map()
+ 
  
 while not libtcod.console_is_window_closed():
  
-    #draw all objects in the list
-    for object in objects:
-        object.draw()
+    #render the screen
+    render_all()
  
-    #blit the contents of "con" to the root console and present it
-    libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
     libtcod.console_flush()
  
     #erase all objects at their old locations, before they move
