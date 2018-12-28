d4948fd8340765179aacb0d7713f9cf23efa042c
Phil <pgrenon.pg@gmail.com>
2018-12-27 23:30:32 -0500

    End of revised tutorial

--- Objects/Player.py
+++
@@ -1,9 +0,0 @@
-from Objects.Entity import Entity
-
-class Player(Entity):
-   '''
-   This is the entity the player will control
-   '''
-   def __init__(self):
-         super()
-         print("Hello")
