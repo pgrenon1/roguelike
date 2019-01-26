c7e5519a9f0c6a0603ee3326f0d24b620c50e86c
Alvaro Salvagno <alvarosalvagno@alvaros-mbp.wireless.concordia.ca>
2019-01-25 21:19:37 -0500

    Started implementing config

---
+++ config/config.py
@@ -0,0 +1,6 @@
+class config():
+
+    SCREEN_WIDTH = 80
+    SCREEN_HEIGHT = 50
+    MAP_WIDTH = 80
+    MAP_HEIGHT = 45
--- engine.py
+++ engine.py
@@ -1,4 +1,5 @@
 import tcod as libtcod
+from config import config
 
 from entity import Entity
 from input_handlers import handle_keys
@@ -7,27 +8,23 @@ from render_functions import clear_all, render_all
 
 
 def main():
-    screen_width = 80
-    screen_height = 50
-    map_width = 80
-    map_height = 45
 
     colors = {
         'dark_wall': libtcod.Color(0, 0, 100),
         'dark_ground': libtcod.Color(50, 50, 150)
     }
 
-    player = Entity(int(screen_width / 2), int(screen_height / 2), '@', libtcod.white)
-    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), '@', libtcod.yellow)
+    player = Entity(int(config.config.SCREEN_HEIGHT / 2), int(config.SCREEN_HEIGHT / 2), '@', libtcod.white)
+    npc = Entity(int(config.SCREEN_HEIGHT / 2 - 5), int(config.SCREEN_HEIGHT / 2), '@', libtcod.yellow)
     entities = [npc, player]
 
     libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 
-    libtcod.console_init_root(screen_width, screen_height, 'libtcod tutorial revised', False)
+    libtcod.console_init_root(config.SCREEN_WIDTH, config.SCREEN_HEIGHT, 'libtcod tutorial revised', False)
 
-    con = libtcod.console_new(screen_width, screen_height)
+    con = libtcod.console_new(config.SCREEN_WIDTH, config.SCREEN_HEIGHT)
 
-    game_map = GameMap(map_width, map_height)
+    game_map = GameMap(config.MAP_WIDTH, config.MAP_HEIGHT)
 
     key = libtcod.Key()
     mouse = libtcod.Mouse()
@@ -35,7 +32,7 @@ def main():
     while not libtcod.console_is_window_closed():
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
 
-        render_all(con, entities, game_map, screen_width, screen_height, colors)
+        render_all(con, entities, game_map, config.SCREEN_WIDTH, config.SCREEN_HEIGHT, colors)
 
         libtcod.console_flush()
 
