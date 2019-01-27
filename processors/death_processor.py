c26b1285d1279b0525a46f99de20efda64b01459
Alvaro Salvagno <alvarosalvagno@Alvaros-MacBook-Pro.local>
2019-01-27 02:13:04 -0500

    ok

--- processors/death_processor.py
+++ processors/death_processor.py
@@ -1,16 +1,17 @@
 import esper
 from components.speed import Speed
 from components.position import Position
-from components.dropsdna import DropsDna
+from components.render import Render
 from components.dna import Dna
 from components.block import Block
 import engine
 
 
-class DropDnaProcessor(esper.Processor):
+class DeathProcessor(esper.Processor):
     def __init__(self):
         super().__init__()
 
     def process(self):
-        for ent, (drops, dna, pos) in self.world.get_components(DropsDna, Dna, Position):
-            print(Dna)
+        for ent, (pos, ren) in self.world.get_components(Position, Render):
+            ren.character = ren.corpse_character
+            
