ab65453346c8be59c7ce88bdffba8a62b1293c36
Phil <pgrenon.pg@gmail.com>
2019-01-26 22:49:14 -0500

    console

---
+++ processors/render_processor.py
@@ -0,0 +1,16 @@
+import esper
+import tcod as libtcod
+
+
+class RenderProcessor(esper.Processor):
+    def __init__(self, console, clear_color):
+        super().__init__()
+        self.console = console
+        self.clear_color = clear_color
+
+    def process(self):
+        libtcod.console_clear(self.console)
+        for ent, (ren, pos) in self.world.get_components(Render, Position):
+            libtcod.console_put_char(
+                self.console, pos.x, pos.y, ren.character, self.clear_color)
+        libtcod.console_flush()
