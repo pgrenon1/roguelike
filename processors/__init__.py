b02810e1439ade71ceca3e7cc88aa7ebcafc48ca
Alvaro <soadzombi@gmail.com>
2019-01-28 19:34:56 -0500

    We have a scene manager and a gameplay scene

---
+++ processors/__init__.py
@@ -0,0 +1,23 @@
+import config
+# We need to learn how to do this correctly
+from processors.ai_processor import AiProcessor
+from processors.damage_processor import DamageProcessor
+from processors.death_processor import DeathProcessor
+from processors.dna_absorb_processor import DnaAbsorberProcessor
+from processors.movement_processor import MovementProcessor
+from processors.render_processor import RenderProcessor
+from processors.spawner_processor import SpawnerProcessor
+from scene_manager import SceneManager
+
+PROCESSOR_GROUP = {
+    'player_turn': [
+        RenderProcessor(config.con, config.COLORS['none']),
+        MovementProcessor(),
+        DeathProcessor()
+    ],
+    'enemy_turn': [
+        # Most likely we need to separate this into EnemyMovementProcessor()
+        MovementProcessor()
+
+    ]
+}
--- scene_gameplay.py
+++ scene_gameplay.py
@@ -1,10 +1,27 @@
 import esper
+import processors
 from scene import Scene
 
 
 class Gameplay(Scene):
-    def __init__(self, world):
+    def __init__(self, world=None):
+        print("Gameplay scene initialized")
         self.world = world
 
+        self.processor_group = processors.PROCESSOR_GROUP
+        self.change_processors('player_turn')
+
         if world is None:
-            esper.CachedWorld()
+            """We can use esper.CachedWorld to get the last world that was assigned to esper (not 100% sure)"""
+            """We should set self.world = esper.World() to whatefver is the first scene we start with I think"""
+            # esper.CachedWorld()
+            self.world = esper.World()
+
+    def change_processors(self, state):
+        self.world_processors = self.processor_group[state]
+        for processor_instance in self.processor_group[state]:
+            processor_instance.world = self.world
+            processor_instance.scene = self
+
+    def update(self):
+        self.world.process()
