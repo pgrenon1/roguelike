790be42cd9c72c99227153ef8d04f8fad0388a15
Phil <pgrenon.pg@gmail.com>
2019-02-15 18:41:41 -0500

    fixed fuck up with the merges

--- components.py
+++ components.py
@@ -71,6 +71,7 @@ class Renderable:
         self.foreground_color = libtcod.Color(*foreground_color)
         self.background_color = libtcod.Color(*background_color)
         self.render_order = RenderOrder[render_order].value
+        self.is_visible = False
 
 
 class Movable:
