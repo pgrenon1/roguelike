1352c9b7c01f5672271546eab933833022e7f3bd
Phil <pgrenon.pg@gmail.com>
2019-03-10 22:36:04 -0400

    what?

--- helpers.py
+++
@@ -1,7 +0,0 @@
-"""This class will contain generic helpers that are meant to help us with our programming, debugging, etc"""
-import config
-
-
-def debug(arg):
-    if(config.VERBOSE_MODE):
-        print(arg)
--- map_generation/tile.py
+++
@@ -1,8 +0,0 @@
-class Tile:
-    """
-    A tile on a map. It may or may not be blocked, and may or may not block sight.
-    """
-
-    def __init__(self, meta_blocks):
-        self.meta_blocks = meta_blocks
-        self.entities = []
